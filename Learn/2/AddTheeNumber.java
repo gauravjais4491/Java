bdda048298dae9db07256e4c087f721a594ccd9e
Noob_Hacker <gauravjais4491@gmail.com>
2022-10-08 00:58:49 +0530

    One

--- Learn/2/AddTheeNumber.java
+++
@@ -1,9 +0,0 @@
-public class AddTheeNumber {
-    public static void main(String[] args) {
-        System.out.println("The sum of Three numbers is: ");
-        int a = 1, b = 2, c = 3;
-        System.out.println(a + b + c);
-
-    }
-
-}
