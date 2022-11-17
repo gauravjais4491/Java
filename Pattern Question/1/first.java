2f351c8e71d3a01afc926761405ded333045a313
Noob_Hacker <gauravjais4491@gmail.com>
2022-11-17 23:47:29 +0530

    xyz

---
+++ Pattern Question/1/first.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of rows: ");
+        int row=sc.nextInt();
+        System.out.print("Enter no. of column: ");
+        int column=sc.nextInt();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
