548be66cb2f1d036ad46c0efdc78a304860ec6f3
Noob_Hacker <gauravjais4491@gmail.com>
2022-11-18 11:25:00 +0530

    input from user

---
+++ Leetcode/1/input.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+//1929. Concatenation of Array
+
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int [] ans=new int[nums.length*2];
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+        }
+        return ans;
+    }
+}
+public class input{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter length of an array: ");
+        int length_of_array=sc.nextInt();
+        int[] nums=new int[length_of_array];
+        for(int i=0;i<nums.length;i++)
+        {
+            System.out.print("Enter "+i+" element of array: ");
+            nums[i]=sc.nextInt();
+        }
+        Solution s=new Solution();
+        int [] ans=s.getConcatenation(nums);
+        for(int a:ans)
+        {
+            System.out.print(a+" ");
+        } 
+        sc.close();
+    }
+}
+
---
+++ Leetcode/2/input.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+// 1920. Build Array from Permutation
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
+public class input{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter length of an array: ");
+        int length_of_array=sc.nextInt();
+        int[] nums=new int[length_of_array];
+        for(int i=0;i<nums.length;i++)
+        {
+            System.out.print("Enter "+i+" element of array: ");
+            nums[i]=sc.nextInt();
+        }
+        Solution s=new Solution();
+        int [] ans=s.buildArray(nums);
+        for(int a:ans)
+        {
+            System.out.print(a+" ");
+        } 
+        sc.close();
+    }
+}
