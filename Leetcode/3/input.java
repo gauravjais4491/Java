57c9169bd94363ce434ea886c165eb6a606b0426
Noob_Hacker <gauravjais4491@gmail.com>
2022-11-18 12:21:51 +0530

    Add Two Integers

---
+++ Leetcode/3/input.java
@@ -0,0 +1,22 @@
+// 2235. Add Two Integers
+
+import java.util.Scanner;
+
+class Solution{
+    public int Add_Two_Interger(int num1,int num2){
+        return num1+num2;
+    }
+}
+public class input {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter First number: ");
+        int num1=sc.nextInt();
+        System.out.print("Enter Second number: ");
+        int num2=sc.nextInt();
+        int sum=s.Add_Two_Interger(num1, num2);
+        System.out.println("The sum of "+num1+" and "+num2+" is: "+sum);
+        sc.close();
+    }   
+}
