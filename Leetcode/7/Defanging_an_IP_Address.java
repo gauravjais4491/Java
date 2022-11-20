4728053a8144287014a1857aef50b0c8400726a1
Noob_Hacker <gauravjais4491@gmail.com>
2022-11-20 18:00:41 +0530

    1108. Defanging an IP Address

---
+++ Leetcode/7/Defanging_an_IP_Address.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution {
+    public String defangIPaddr(String address) {
+        return address.replace(".","[.]");
+    }
+}
+
+public class Defanging_an_IP_Address {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter an IP Address: ");
+        String IP_Address=sc.next();
+        System.out.println(s.defangIPaddr(IP_Address)); 
+        sc.close();
+    }
+}
